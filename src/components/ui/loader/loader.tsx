0f48c997418d415e9f813247bada1f2940b0598d
ammarelbadry1 <ammarelbadry1@gmail.com>
2026-02-04 03:16:05 +0200

    feat(loader): Add size variants and wrapper option

--- src/components/ui/loader/loader.tsx
+++ src/components/ui/loader/loader.tsx
@@ -1,9 +1,28 @@
 import styles from "./loader.module.css";
+import clsx from "clsx";
 
-export const Loader = () => {
-  return (
-    <div className={styles["wrapper"]}>
-      <div className={styles["loader"]}></div>
-    </div>
+interface LoaderProps {
+  size?: "small" | "medium" | "large";
+  showWrapper?: boolean;
+}
+
+export const Loader = ({
+  size = "medium",
+  showWrapper = true,
+}: LoaderProps) => {
+  const loaderSizeClass = clsx(
+    size === "small" && styles["loader-small"],
+    size === "medium" && styles["loader"],
+    size === "large" && styles["loader-large"],
   );
+
+  if (showWrapper) {
+    return (
+      <div className={styles["wrapper"]}>
+        <div className={loaderSizeClass}></div>
+      </div>
+    );
+  }
+
+  return <div className={loaderSizeClass}></div>;
 };
