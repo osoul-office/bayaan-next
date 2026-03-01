c3e943f3140ba354a183e05bf65172390bfc8519
ammarelbadry1 <ammarelbadry1@gmail.com>
2026-03-01 15:10:15 +0200

    feat(portal): add accounts page with data table

--- src/app/portal/accounts/page.tsx
+++ src/app/portal/accounts/page.tsx
@@ -1,11 +1,63 @@
 import { Actions } from "@/components/ui";
 import { paths } from "@/config/paths";
+import { Table, type ColumnDefinition } from "@/components/ui";
+import { SelectionProvider } from "@/providers";
+import { getRelativeTimeAgo } from "@/utils";
+import * as accountService from "@/services/account";
+import { Account as AccountT } from "@/types";
 
-export default async function Account() {
+interface ClientAccountT extends Omit<AccountT, "updatedAt"> {
+  updatedAt: string;
+}
+
+export default async function Account({
+  searchParams,
+}: {
+  searchParams: {
+    page?: string;
+    length?: string;
+  };
+}) {
+  const page = Number(searchParams?.page) || 1;
+  const length = Number(searchParams?.length) || 20;
+
+  const accounts = await accountService.get();
+
+  if (accounts.isFailure) {
+    throw new Error(accounts.error);
+  }
+
+  const columns: ColumnDefinition<ClientAccountT>[] = [
+    {
+      key: "id",
+      header: "ID",
+    },
+    {
+      key: "name",
+      header: "Name",
+    },
+    {
+      key: "currentBalance",
+      header: "Balance",
+    },
+    {
+      key: "updatedAt",
+      header: "Activity",
+    },
+  ];
+
+  const processedAccounts = accounts.value.map((account) => ({
+    ...account,
+    updatedAt: getRelativeTimeAgo(account.updatedAt),
+  }));
+
   return (
     <>
       <h1>Accounts</h1>
-      <Actions create={paths.portal.accounts.create.getHref()} />
+      <SelectionProvider>
+        <Actions create={paths.portal.accounts.create.getHref()} />
+        <Table columns={columns} data={processedAccounts} />
+      </SelectionProvider>
     </>
   );
 }
